201d0ec4764d17c4ba21a9fb61f2ee90666c7123
kwang-zzang <j2h2677@gmail.com>
2019-05-15 09:38:46 +0900

    boardController 작성

---
+++ src/main/java/com/board/controller/BoardController.java
@@ -0,0 +1,25 @@
+package com.board.controller;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.board.service.BoardService;
+
+@Controller
+@RequestMapping(value = "/")
+public class BoardController {
+	
+	@Inject
+	private BoardService service;
+	
+	@RequestMapping(value="/listAll", method= RequestMethod.GET )
+	public void listAll(Model model) throws Exception {
+		
+		model.addAttribute("list", service.listAll());
+	}
+
+}
