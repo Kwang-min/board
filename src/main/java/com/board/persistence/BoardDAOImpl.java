7b4030671fa9478b5f474d042620cf204d2b28ba
kwang-zzang <j2h2677@gmail.com>
2019-05-15 14:28:21 +0900

    BoardDAO 수정

--- src/main/java/com/board/persistence/BoardDAOImpl.java
+++ src/main/java/com/board/persistence/BoardDAOImpl.java
@@ -15,12 +15,12 @@ public class BoardDAOImpl implements BoardDAO {
 	@Inject
 	private SqlSession session;
 	
-	private static String namespace = "com.board.domain.BoardVO";
+	private static String namespace = "com.myp.mapper.BoardMapper";
 	
 	@Override
 	public void create(BoardVO vo) throws Exception {
 		// TODO Auto-generated method stub
-		session.insert(namespace+"create",vo);
+		session.insert(namespace+".create",vo);
 	}
 
 	@Override
